eb578383733193c2ceba3762e80be64e7f0315af
Helen <elenaprofessor@yandex.ru>
2022-09-04 14:53:17 +0300

    34 задача

---
+++ масс 1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве
+Console.WriteLine("Введите размер массива ");
+int get = int.Parse(Console.ReadLine());
+int sum = 0;
+int[]array = new int[get];
+for (int i=0; i<array.Length;i++){
+    array[i] = new Random().Next(100,1000);
+    }
+for (int i = 0;i<array.Length; i++){
+    sum = sum+array[i]%2;
+    }
+    Console.WriteLine ("массив ["+ string.Join(",", array)+ "]" +"=>"+ (sum) + "четных числа");
